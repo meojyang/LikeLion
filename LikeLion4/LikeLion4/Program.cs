8e4a09df713bd28100e17ef304716caf8e903123
Guy Kim <khk3482@gmail.com>
2025-02-21 14:34:44 +0900

    Study5

---
+++ LikeLion4/LikeLion4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ctrl + / 하면 드래그 한곳 주석 하는 단축키
+            // Ctrl + shift + / 하면 드래그 한곳 전체 주석하는 단축키 /**/ 이거
+
+            /*string greeting; // 문자열 변수를 선언
+            greeting = "Hello, World!"; // 변수에 헬로월드 값을 저장
+
+            Console.WriteLine(greeting); //greeting에 저장된 값을 출력
+*/
+
+            /*// 변수 선언과 초기화를 동시에
+
+            int score = 100; //score라는 int형 변수를 선언하면서 100이라는 값으로 초기화함
+            double temperature = 36.5;
+            string city = "seoul";
+
+            Console.WriteLine(score);
+            Console.WriteLine(temperature);
+            Console.WriteLine(city);
+*/
+
+            /*// 같은 데이터 타입의 변수를 쉼표로 구분해서 동시에 선언
+            int x = 10, y = 20, z = 30;
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);*/
+
+            /*const double Pi = 3.14159;
+            const int MaxScore = 100;
+
+            Console.WriteLine("Pi : " + Pi);
+            Console.WriteLine("MaxScore  : " + MaxScore);*/
+
+            int att = 16755;
+            int maxHP = 78103;
+            int cri = 36;
+            int major = 1017;
+            int supp = 41;
+            int speed = 611;
+            int endure = 22;
+            int exp = 39;
+
+            Console.WriteLine("기본 특성");
+            Console.WriteLine("공격력 : " + att);
+            Console.WriteLine("최대 생명력 : " + maxHP + '\n');
+            Console.WriteLine("전투 특성");
+            Console.WriteLine("치명 : " + cri);
+            Console.WriteLine("특화 : " + major);
+            Console.WriteLine("제압: " + supp);
+            Console.WriteLine("신속 : " + speed);
+            Console.WriteLine("인내 : " + endure);
+            Console.WriteLine("숙련 : " + exp);
+        }
+    }
+}
---
+++ LikeLion5/LikeLion5/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int intgerNum = 10;
+            float floatNum = 3.14f;
+            double doubleNum = 3.14159;
+
+            Console.WriteLine(intgerNum);
+            Console.WriteLine(floatNum);
+            Console.WriteLine(doubleNum);
+*/
+            /*//정수 데이터타입
+            int intValue = -100;
+            long longValue = 1239802130;
+
+            Console.WriteLine(intValue);
+            Console.WriteLine(longValue);
+*/
+
+            /*//부호 있는 정수
+            sbyte signedByte = -50;
+            short signedShort = -32000;
+            int signedInt = -2000000000;
+
+            Console.WriteLine(signedByte);
+            Console.WriteLine(signedShort);
+            Console.WriteLine(signedInt);
+*/
+            /*//부호 없는 정수
+
+            byte unsignedByte = 255;
+            ushort unsignedShort = 65000;
+            uint unsignedInt = 4000000000;
+
+            Console.WriteLine(unsignedByte);
+            Console.WriteLine(unsignedShort);
+            Console.WriteLine(unsignedInt);
+            */
+
+
+            /*//실수형 데이터
+            float singlePrecision = 3.14f;
+            double doublePrecision = 3.1415926535;
+            decimal highPrecision = 3.1415926535897932384626433833m;
+
+            Console.WriteLine(singlePrecision);
+            Console.WriteLine(doublePrecision);
+            Console.WriteLine(highPrecision);
+*/
+
+            //접미사를 사용해서 숫자의 데이터 형식을 명시한다
+            /*int intValue = 100;
+            long longValue = 100L;
+            float floatValue = 3.14f;
+            double doubleValue = 3.14;
+            decimal decimalValue = 3.14m;
+
+            Console.WriteLine(intValue);
+            Console.WriteLine(longValue);
+            Console.WriteLine(floatValue);
+            Console.WriteLine(doubleValue);
+            Console.WriteLine(decimalValue);*/
+
+
+
+            /*//char 형식: 단일 문자를 저장할 수 있다
+            char letter = 'A';
+            char symbol = '#'; // 특수 기호 #을 저장
+            char number = '9'; // 문자 9를 저장, 9라는 값을 저장하는 것이 아님
+
+            Console.WriteLine(letter);
+            Console.WriteLine(symbol);
+            Console.WriteLine(number);*/
+
+            /*//string 형식: 문자열을 저장할 수 있다
+            string greeting = "Hello, World!";
+            string name = "Alice";
+
+            Console.WriteLine(greeting + " " + name);
+*/
+
+            /*
+            //bool 형식: 참 또는 거짓의 값을 저장할 수 있다
+
+            bool isRunning = true;
+            bool isFinished = false;
+
+            Console.WriteLine(isRunning);
+            Console.WriteLine(isFinished);
+            */
+
+            /*
+            //const: 변하지 않는 상수를 저장할 수 있다
+
+            const double Pi = 3.14159;
+            const int MaxScore = 100;
+
+            Console.WriteLine(Pi);
+            Console.WriteLine(MaxScore);
+            */
+
+            /*
+            //닷넷형식: 기본형식의 닷넷 표현?
+
+            System.Int32 number = 123; //int의 닷넷 형식
+            System.String text = "Hello"; //string의 닷넷 형식
+            System.Boolean flag = true; //bool의 닷넷 형식
+
+            Console.WriteLine(number);
+            Console.WriteLine(text);
+            Console.WriteLine(flag);
+            */
+            
+            /*
+            //래퍼 형식: 기본 데이터 형식을 클래스 형태로 감싸 객체취급할수있게함
+            //int 래퍼 형식의 메서드 활용
+            int num = 123;
+            string numberAsString = num.ToString(); //123이라는 숫자를 문자로 바꿔 초기화
+
+            //bool 래퍼 형식의 메서드 활용
+
+            bool flag = true;
+            string flagAsString = flag.ToString(); //true값을 문자열로 바꿔 string 문자열에 넣어 초기화
+
+            Console.WriteLine(numberAsString); //123 문자열 출력
+            Console.WriteLine(flagAsString); //true 문자열 출력
+            */   
+
+
+
+
+        }
+    }
+}
