42421e0be6fc15aeb4f2707a7254d9434dbaa763
Guy Kim <khk3482@gmail.com>
2025-02-21 09:57:12 +0900

    study1

---
+++ LikeLion1/LikeLion1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("깃허브 연결하기");
+        }
+    }
+}
