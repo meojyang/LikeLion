d14181df92e3afe5c242be6dae4f099d1f6b6379
Guy Kim <khk3482@gmail.com>
2025-02-24 15:13:25 +0900

    논리연산 비트연산

---
+++ LikeLion8/LikeLion8/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*int b = 3;
+            b++;
+
+            //전위 ++b , 후위 b++
+
+            Console.WriteLine("b의 값은 : " + (++b));*/
+
+            /*int x = 5, y = 10;
+
+            Console.WriteLine(x < y);       // true
+            Console.WriteLine(x == y);     //false
+            Console.WriteLine(x != y);      // true
+            Console.WriteLine(x > y);       //false
+            Console.WriteLine(x >= y);      //false
+            Console.WriteLine(x <= y);      //true*/
+
+
+            // 논리연산자
+            /*bool a = true;
+            bool b = false;
+
+            Console.WriteLine(a || b);
+            Console.WriteLine(a && b);
+            Console.WriteLine(!a);*/
+
+            //비트연산
+
+            /*int x = 5; // 0101
+            int y = 3; // 0011
+
+
+
+            Console.WriteLine(x & y);  // 0001  = 1
+            Console.WriteLine(x | y);  //0111  = 7
+            Console.WriteLine(x ^ y); // 0110 = 6
+            Console.WriteLine(~x); // 1010 = -6*/
+
+            /*int a = 10, b = 20;
+
+            int max;
+
+            max = (a < b) ? a : b; //삼항 연산자
+
+            Console.WriteLine(max);*/
+
+
+            /* int key = 1;
+
+             string str;
+             str = (key == 2) ? "문이열렸습니다." : "문을 못열었습니다.";
+
+             Console.WriteLine(str);*/
+
+            /*int result = 10 + 2 * 5;  //연산자 우선순위가 곱셈이 더 높기 때문에 2*5를 먼저 계산
+            Console.WriteLine(result); //결과는 10 + 10이 되어 20이 출력될 것
+
+            int adjustedResult = (10 + 2) * 5;  // 괄호가 곱셈보다 우선순위가 더 높아 12 * 5 계산
+            Console.WriteLine(adjustedResult);  // 60이 출력될것
+*/
+
+
+
+
+        }
+    }
+}
