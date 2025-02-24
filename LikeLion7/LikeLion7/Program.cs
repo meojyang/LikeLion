1bed4add76f1c00288cf6d378242f0b772313f64
Guy Kim <khk3482@gmail.com>
2025-02-24 11:56:00 +0900

    Study7

---
+++ LikeLion7/LikeLion7/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*//이진수를 정수로 변환
+            Console.Write("2진수를 입력하세요:");
+            string binaryInput = Console.ReadLine();  //입력받는다 문자열
+            int decimalValue = Convert.ToInt32(binaryInput, 2); //2진수 -> 10진수 변환
+
+            //정수를 이진수로 변환
+            string binaryOutput = Convert.ToString(decimalValue, 2);//10진수 ->2진수
+
+            Console.WriteLine($"입력한 이진수: {binaryInput}");
+            Console.WriteLine($"10진수로변환 : {decimalValue}");
+            Console.WriteLine($"다시 이진수로 변환 :{binaryOutput}");*/
+            /*
+                        var name = "Alice";
+                        var age = 25;
+                        var isStudent = true;
+
+                        Console.WriteLine($"이름: {name}, 나이: {age}, 학생 여부: {isStudent}");
+            */
+
+            /*int defaultInt = default;
+            string defaultString = default;
+            bool defaultBool = default;
+
+            Console.WriteLine($"정수 기본값: {defaultInt}");
+            Console.WriteLine($"문자열 기본값: {defaultString}");
+            Console.WriteLine($"논리값 기본값: {defaultBool}");*/
+
+            /*int a = 5, b = 3;
+            int sum = a + b;
+            bool isEqual = (a == b);
+
+            Console.WriteLine($"a와 b의 합: {a} + {b} = {sum}");
+            Console.WriteLine($"A와 B가 같은가? : {isEqual}");
+*/
+
+            /* int number = 7;
+
+
+             int sum = 0;
+
+
+             sum = number % 2;  // 0나오면 짝수   1 홀수
+
+             Console.WriteLine("짝수 홀수 판별 : " + sum);*/
+
+            /* bool isEqual = false;
+             int a = 5;
+             int b = 5;
+
+             isEqual = (a == b);
+             Console.WriteLine($"같은가?: {isEqual}");*/
+
+            /*int num = 5;
+
+            Console.WriteLine(+num);
+            Console.WriteLine(-num);
+
+            bool flag = true;
+            Console.WriteLine(!flag);*/
+
+
+            /*int num = 10;
+            int result = ~num;   //모든 비트 반전 : 1111 0101 결과 
+
+
+            Console.WriteLine("원래 값 : " + num);
+            Console.WriteLine("~ 연산자 적용 후: " + result);*/
+
+            /*int score_kor = 37;
+            int score_eng = 66;
+            int score_math = 72;
+
+            int sum = 0;
+            float average = 0.0f;
+
+            sum = score_kor + score_eng + score_math;
+
+
+            average = (float)sum / 3;
+
+            Console.WriteLine("총점 : " + sum);
+            Console.WriteLine("평균 : " + average);*/
+
+            /*int a = 10, b = 3;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);*/
+
+            /*string firstName = "Alice";
+            string lastName = "Smith";
+
+            Console.WriteLine(firstName + " " + lastName); //출력*/
+
+            int a = 10;
+            a += 5;
+
+            Console.WriteLine(a);
+
+
+        }
+    }
+}
