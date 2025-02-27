9814071801837faae7bbf5596d59a97f5ca334db
Guy Kim <khk3482@gmail.com>
2025-02-27 13:23:10 +0900

    구조체

---
+++ LikeLion17/LikeLion17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion17
+{
+
+    //구조체
+    struct Point
+    {
+        public int x;
+        public int y;
+
+        public void Print()
+        {
+            Console.WriteLine($"x좌표: {x}, y좌표: {y}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point p;
+            p.x = 10;
+            p.y = 20;
+
+            p.Print();
+        }
+    }
+}
