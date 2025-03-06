79b1f9b234e0dbcb4e4940061c66fa83c03fdcc8
Guy Kim <khk3482@gmail.com>
2025-03-06 10:07:24 +0900

    생성자와 소멸자, 필드, 네임스페이스

---
+++ LikeLion30/LikeLion30/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion30
+{
+    class MyResource
+    {
+        ~MyResource() //소멸자
+        {
+            Console.WriteLine("삭제될때 호출");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyResource r = new MyResource();
+            //GC에 의해 나중에 소멸자 호출
+        }
+    }
+}
