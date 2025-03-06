d467c6fe152db7f872eb06fa9860014fb52a86ed
Guy Kim <khk3482@gmail.com>
2025-03-06 11:16:26 +0900

    자식클래스의 부모생성자 호출

---
+++ LikeLion34/LikeLion34/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion34
+{
+    class Parent
+    {
+        protected string name;
+
+        //부모 생성자에서 name 초기화
+        public Parent(string name)
+        {
+            this.name = name;
+            Console.WriteLine($"부모 생성자: {name}");
+        }
+    }
+
+    class Child : Parent
+    {
+        private int age;
+
+        //부모생성자를 호출하며 name은 전달하고 age는 초기화
+        public Child(string name, int age):base(name)
+        {            
+            this.age = age;
+            Console.WriteLine($"자식 생성자, 나이: {age}");
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"이름: {name}, 나이: {age}");
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child("홍길동", 23);
+            child.ShowInfo();
+        }
+    }
+}
